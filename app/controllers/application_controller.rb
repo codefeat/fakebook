78b679608a6ef663aeca3f00d2ea7424b4e6dda5
Jeremy Mauzy <apositivejam@users.noreply.github.com>
2015-07-06 00:39:50 -0500

    Updated home page layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_login
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
@@ -10,4 +11,12 @@ protected
     	devise_parameter_sanitizer.for(:sign_up) << :last_name
   	end
 
+private
+
+	def require_login
+		unless current_user
+			flash[:error] = "You must log in to use the site"
+			redirect_to root_path
+		end
+	end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-	
+	skip_before_action :require_login
 	def home
 		if user_signed_in?
 			redirect_to timeline_path
