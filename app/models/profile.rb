ecc7f9ded8991421f28b3592d00610b9656ccb32
Jeremy Mauzy <apositivejam@users.noreply.github.com>
2015-07-07 20:25:49 -0500

    updated paperclip validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,6 +5,7 @@ class Profile < ActiveRecord::Base
 	#Settings for Paperclip (used for avatar)
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, 
   			default_url: "/images/:style/missing.png"
-  	validates_attachment_content_type :avatar, content_type: /\Aimage\/.\Z/
+  	validates_attachment :avatar, content_type: { content_type: /^image\/(jpe?g|png|gif)$/,
+                      message: "must be .jpeg, .jpg, .png, or .gif" }
 
 end
